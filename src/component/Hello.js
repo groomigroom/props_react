963d56be63c04bdaa1766c2b92bfe956dd316466
groomigroom <googjohn0143@gmail.com>
2026-03-15 11:09:44 +0000

    hello use

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,7 +1,12 @@
-export default function Hello(){
+import { useState } from "react";
+
+export default function Hello(props){
+    console.log(props);
+    const [name, setName] = useState("Groom");
+
     return (
-        <>
+        <div>
         
-        </>
+        </div>
     );
 }
